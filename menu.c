230f5e1eed41cb86b1a7d5b795460d1ecf37f236
git config --global user.name charangit config --global user.email charanani128@gmail.com <charanani128@gmail.comgit config --global user.name charanmhhfgit config --global user.name sadumcharangit config --global user.email charanani128@gmail.com>
2026-06-17 16:06:36 +0000

    second commit

--- menu.c
+++ menu.c
@@ -1,34 +1,93 @@
 #include<stdio.h>
 int main()
 {
-    char op;
-    printf("--------Welcome--------"\n);
+    char op,ch;
+    
+    printf("--------Welcome--------\n");
     printf("Select what you want ->\n");
-    printf("1.starter\n2.Main course\n3.Exit\n");
+    printf("1.starter\n2.Main course\n3.desert\n4.Exit\n");
 
-    scanf("%c", &op);
+    scanf(" %c",&op);
 
     switch(op)
     {
         case '1':
         printf("you have choosed starter\n");
-        printf("1. \n2.  \n  3. \n");
-
+        printf("1.Chicken Tikka\n2.Tandoori Chicken\n3.Dragon Chicken\n4.Chilli Chicken\n");
+        scanf(" %c",&ch);
+        switch(ch)
+        
+        {
+            case '1':
+                    printf("Your Order is Chicken Tikka\n");
+                    break;
+            case '2':
+                    printf("Your Order is Tandoori Chicken\n");
+                    break;
+            case '3':
+                    printf("Your Order is Dragon Chicken\n");
+                    break;
+            case '4':
+                    printf("Your Order is Chill Chicken\n");
+                    break;
+            default:
+                printf("Invalid Choice\n");        
+        }
+        break;
+        
         case '2':
         printf("you have choosed Main course\n");
-        printf("1. \n2.  \n  3. \n")
+        printf("1.Hyberabadi Chicken Biryani\n2.Mutton Biryani\n3.Butter Naan\n4.Butter Chicken\n");
+        scanf(" %c", &ch);
+    
+        switch(ch)
+        {
+            case '1':
+                    printf("Your Order is Hyberabadi Chicken Biryani\n");
+                    break;
+            case '2':
+                    printf("Your Order is Mutton Biryani\n");
+                    break;
+            case '3':
+                    printf("Your Order is Butter Naan\n");
+                    break;
+            case '4':
+                    printf("Your Order is Butter Chicken\n");
+                    break;
+            default:
+                printf("Invalid Choice\n");
+        }
+        break;
 
         case '3':
         printf("you have choosed Desert\n");
-        printf("1. \n2.  \n  3. \n")
+        printf("1.Gulab Jamun\n2.Rasmalai\n3.Kulfi\n4.Lava Cake\n");
+        scanf(" %c", &ch);
+        switch(ch)
+        {
+            case '1':
+                    printf("Your Order is Gulab Jamun\n");
+                    break;
+            case '2':
+                    printf("Your Order is Rasmalai\n");
+                    break;
+            case '3':
+                    printf("Your Order is Kulfi\n");
+                    break;
+            case '4':
+                    printf("Your Order is Lava Cake\n");
+                    break;
+            default:
+                printf("Your Order is Invalid Choice\n");
+        
+        }
+        break;
 
         case '4':
-        printf("you have choosed starter\n");
-        printf("1. \n2.  \n  3. \n")
+                exit(0);
+               
 
-        case '5':
-        printf("Thank you visit again\n");
-        break;
+        
 
         default:
         printf("Sorry,try next time\n");
